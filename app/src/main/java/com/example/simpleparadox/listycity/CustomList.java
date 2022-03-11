1d6aa04b878d84c4d2b162761151ba564ac259a6
xiaoluzzz <zxl9487@gmail.com>
2022-03-10 22:59:08 -0700

    finish countCities

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -68,7 +68,6 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int countCities() {
-//        return cities.size();
-        return 0;
+        return cities.size();
     }
 }
